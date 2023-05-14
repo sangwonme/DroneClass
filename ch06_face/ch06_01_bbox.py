bcd4de4a0279ce28cb1c855e7a0847f6d2dca3af
sangwonme <paulmoguri@naver.com>
2023-05-14 12:03:17 +0900

    :art: Add bbox

--- ch06_face/ch06_01_bbox.py
+++ ch06_face/ch06_01_bbox.py
@@ -2,6 +2,7 @@ import cv2
 
 # Load the cascade
 face_cascade = cv2.CascadeClassifier(cv2.data.haarcascades + 'haarcascade_frontalface_default.xml')
+print(cv2.data.haarcascades + 'haarcascade_frontalface_default.xml')
 
 # To use a video file instead of a camera, pass the video file path instead of the camera index
 cap = cv2.VideoCapture(0)
@@ -9,6 +10,8 @@ cap = cv2.VideoCapture(0)
 while True:
     # Read the frame
     _, img = cap.read()
+
+    img = cv2.flip(img, 1)
 
     # Convert to grayscale
     gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
@@ -20,7 +23,17 @@ while True:
     for (x, y, w, h) in faces:
         cv2.rectangle(img, (x, y), (x+w, y+h), (255, 0, 0), 2)
 
+        # Calculate the center point
+        center_x = x + w//2
+        center_y = y + h//2
+
+        # Draw the red dot
+        cv2.circle(img, (center_x, center_y), radius=10, color=(0, 0, 255), thickness=-1)
+
+
+
     # Display
+    # img = cv2.flip(img, 1)
     cv2.imshow('img', img)
 
     # Stop if escape key is pressed
