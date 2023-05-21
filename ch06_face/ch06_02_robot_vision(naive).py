90d94ca82eebda80595b7e761d9dcd923e98492c
sangwonme <paulmoguri@naver.com>
2023-05-21 12:51:16 +0900

    :sparkles: Add robot vision (naive ver)

--- ch06_face/ch06_02_robot_vision(naive).py
+++ ch06_face/ch06_02_robot_vision(naive).py
@@ -1,17 +1,7 @@
-# simple example demonstrating how to control a Tello using your keyboard.
-# For a more fully featured example see manual-control-pygame.py
-# 
-# Use W, A, S, D for moving, E, Q for rotating and R, F for going up and down.
-# When starting the script the Tello will takeoff, pressing ESC makes it land
-#  and the script exit.
-
 from djitellopy import Tello
 import cv2, math, time
 
-import matplotlib.pyplot as plt
-import numpy as np
-
-y = []
+face_cascade = cv2.CascadeClassifier(cv2.data.haarcascades + 'haarcascade_frontalface_default.xml')
 
 tello = Tello()
 tello.connect()
@@ -24,9 +14,33 @@ while True:
     # In reality you want to display frames in a seperate thread. Otherwise
     #  they will freeze while the drone moves.
     img = frame_read.frame
-    cv2.imshow("drone", img)
 
-    isMove = True
+    # flip
+    img = cv2.flip(img, 1)
+
+    # Convert to grayscale
+    gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+
+    # Detect the faces
+    faces = face_cascade.detectMultiScale(gray, 1.1, 4)
+
+    # Draw the rectangle around each face
+    for (x, y, w, h) in faces:
+        cv2.rectangle(img, (x, y), (x+w, y+h), (255, 0, 0), 2)
+
+        # Calculate the center point
+        center_x = x + w//2
+        center_y = y + h//2
+
+        # Draw the red dot
+        cv2.circle(img, (center_x, center_y), radius=10, color=(0, 0, 255), thickness=-1)
+
+        # Put the text (coordinate)
+        # load font
+        font = cv2.FONT_HERSHEY_SIMPLEX 
+        cv2.putText(img, '({}, {})'.format(center_x, center_y), (x, y-10), font, 2, (0, 255, 0), 2)
+
+    cv2.imshow("drone", img)
 
     key = cv2.waitKey(1) & 0xff
     if key == 27: # ESC
@@ -50,12 +64,4 @@ while True:
     else:
         isMove = False
     
-    if(isMove):
-        print(tello.get_current_state()['tof'])
-        y.append(tello.get_current_state()['tof'])
-
 tello.land()
-
-y = np.array(y)
-plt.plot(y)
-plt.show()
--- ch07_function/ch07_01_function.py
+++ ch07_function/ch07_01_function.py
@@ -3,7 +3,14 @@
 # return : 만약에 num이 소수이면 true를, 소수가 아니면 false를 return 한다.
 def isPrimeNum(num):
     # TODO
-    pass
+    if num == 2:
+        return True
+    else:
+        for i in range(2, num):
+            if num % i == 0:
+                return False
+            if i == num-1:
+                return True
 
 for i in range(1, 21):
     print('{}는 소수'.format(i), end='')
