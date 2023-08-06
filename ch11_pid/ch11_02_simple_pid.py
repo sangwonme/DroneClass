f8c79ba6db7d3a892c42b1c1b0baf63fa1fc000d
sangwonme <paulmoguri@naver.com>
2023-08-06 11:03:06 +0900

    :sparkles: Add graph

--- ch11_pid/ch11_02_simple_pid.py
+++ ch11_pid/ch11_02_simple_pid.py
@@ -1,6 +1,7 @@
 import time
 from djitellopy import Tello
 from simple_pid import PID
+from matplotlib import pyplot as plt
 
 # PID constants
 Kp = 1.0
@@ -15,6 +16,9 @@ pid = PID(Kp, Ki, Kd, setpoint=target_altitude)
 
 # Create the Tello drone object
 tello = Tello()
+
+# Create Datas to plot
+datas = []
 
 try:
     # Connect to the drone
@@ -36,6 +40,35 @@ try:
         # Sleep for a bit
         time.sleep(0.01)
 
+        # datas
+        datas.append([current_altitude, throttle, target_altitude])
+
 except KeyboardInterrupt:
+    # Unpacking the y-values
+    y1 = [item[0] for item in datas]
+    y2 = [item[1] for item in datas]
+    y3 = [item[2] for item in datas]
+
+    # Generate x-values (assuming each set of y-values corresponds to a new time point)
+    x = list(range(len(datas)))
+
+    # Plotting the data
+    plt.plot(x, y1, label="sensor")
+    plt.plot(x, y2, label="actuation")
+    plt.plot(x, y3, label="target sensor")
+
+    # Setting plot properties
+    plt.title("Y-values over time")
+    plt.xlabel("Time")
+    plt.ylabel("Value")
+    plt.legend()
+
+    plt.show()
+
     # Land the drone when Ctrl+C is pressed
     tello.land()
+
+    
+
+
+    
